b2fae01df3bff0e515c0c8d700046261c61fbc45
kirtimmit9 <kirtimmit@outlook.com>
2026-04-19 18:38:39 +0300

    Update Green.cs

--- Lab9/Green/Green.cs
+++ Lab9/Green/Green.cs
@@ -1,3 +1,27 @@
-﻿namespace Lab9.Green
+using System;
+
+namespace Lab9.Green
 {
+    public abstract class Green
+    {
+        private string _input;
+
+        public string Input
+        {
+            get { return _input; }
+        }
+
+        protected Green(string text)
+        {
+            _input = text;
+        }
+
+        public abstract void Review();
+
+        public virtual void ChangeText(string text)
+        {
+            _input = text;
+            Review();
+        }
+    }
 }
